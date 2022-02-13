eba863af03ba69982eeea23c7615af35ed8b954b
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-13 17:53:19 +0100

    Create localization.ts

---
+++ lib/helpers/localization.ts
@@ -0,0 +1,13 @@
+type PhrasesType = {
+    'Press Enter key to exit the process...': string,
+}
+
+
+const en: PhrasesType = {
+    'Press Enter key to exit the process...': 'Press Enter key to exit the process...',
+}
+
+
+const cs: PhrasesType = {
+    'Press Enter key to exit the process...': 'Proces ukončíte stisknutím klávesy Enter...',
+}
