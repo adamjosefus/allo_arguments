7dba3ce514e480dc84c4e7b9651132851bd711c7
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-14 07:23:22 +0100

    Update demo

--- demo/main.ts
+++ demo/main.ts
@@ -4,14 +4,14 @@ import { Arguments, ValueException } from "../mod.ts";
 
 
 function init() {
-    const configProcessor = (v: string | null): string => {
-        if (v == null) throw new ValueException(`Cesta konfiguračního souboru není nastavena. Nastavíte jí pomocí "--config=<path>"`)
+    const configConvertor = (v: string | null): string => {
+        if (v == null) throw new ValueException(`Path to configuration file is not set. You can set it using "--config=<path>"`)
 
         return join(Deno.cwd(), v);
     }
 
 
-    const sleepProcessor = (v: string | number | null): number | null => {
+    const sleepConvertor = (v: string | number | null): number | null => {
         if (v === null) return null;
         if (typeof v === "string") v = parseInt(v, 10);
 
@@ -21,78 +21,54 @@ function init() {
         return v;
     }
 
-    const booleanProcessor = (v: boolean | string | null) => v === true || v === 'true';
+    const booleanConvertor = (v: boolean | string | null) => v === true || v === 'true';
 
 
     const args = new Arguments(
         {
             name: 'config, c',
-            description: `Cesta na konfigujrační soubor ve formátu JSON.`,
-            processor: configProcessor,
+            description: `Path to the configuration file in JSON format.`,
+            convertor: configConvertor,
             default: 'config.json'
         },
         {
-            name: 'clone',
-            description: `Příznak pro naklonování repozitářů.`,
-            processor: booleanProcessor,
-            default: false,
-        },
-        {
-            name: 'pull',
-            description: `Příznak pro aktualizaci repozitářů.`,
-            processor: booleanProcessor,
-            default: false,
-        },
-        {
             name: 'delete',
-            description: `Smaže všechny složky repozitářů podle konfiguračního souboru.`,
-            processor: booleanProcessor,
+            description: `Deletes all folders according to the configuration file.`,
+            convertor: booleanConvertor,
             default: false,
         },
         {
             name: 'sleep, s',
-            description: `Délka pauzy mezi procesy v milisekundách.`,
-            processor: sleepProcessor,
+            description: `Sleep duration between processes in milliseconds.`,
+            convertor: sleepConvertor,
             default: 5000,
-        },
-        {
-            name: 'pretty-format, prtt',
-            description: `Naformátuje seznamy repozitářů do čitelnější podoby. Soubor bude upraven.`,
-            processor: booleanProcessor,
-            default: false,
-        },
-        {
-            name: 'exitkey',
-            processor: booleanProcessor,
-            default: true,
         },
     );
 
-    args.setDescription(`Nástroj na naklonování a aktualizaci repozitářů.`)
-    args.setVersion('v1.0.0');
+    args.setDescription(`My beautiful program.`);
+
+    args.keepProcessAlive();
 
 
-    // Help
+    // Important for `--help` flag works.
     if (args.shouldHelp()) args.triggerHelpException();
 
 
     const values = {
         config: args.get<string>('config'),
-        clone: args.get<boolean>('clone'),
-        pull: args.get<boolean>('pull'),
         delete: args.get<boolean>('delete'),
         sleep: args.get<number>('sleep'),
-        prettyFormat: args.get<boolean>('pretty-format'),
     }
 
 
     console.log(values);
-    
 }
 
 
 try {
+    // Dont forget to await! (in async scope of course).
     await init();
+
 } catch (error) {
     if (!Arguments.isArgumentException(error)) throw error;
 }
