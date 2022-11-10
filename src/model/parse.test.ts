6bf76a4d59e1eacf84d370cf178a23f690f481db
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-11-10 14:02:21 +0100

    Update parse method

---
+++ src/model/parse.test.ts
@@ -0,0 +1,68 @@
+/**
+ * @copyright Copyright (c) 2022 Adam Josefus
+ */
+
+import { assertEquals } from "../../libs/deno_std/testing_asserts.ts";
+import { parse } from "./parse.ts";
+
+
+Deno.test("parse", () => {
+    assertEquals(parse([]), []);
+
+    assertEquals(parse(["--foo"]), [
+        {
+            _tag: "Flag",
+            name: "foo",
+            order: 0,
+            short: false,
+            value: true,
+        },
+    ]);
+
+    assertEquals(parse(["--key1=value1", "--key2", "value2"]), [
+        {
+            _tag: "Flag",
+            name: "key1",
+            order: 0,
+            short: false,
+            value: "value1",
+        },
+        {
+            _tag: "Flag",
+            name: "key2",
+            order: 1,
+            short: false,
+            value: "value2",
+        },
+    ]);
+
+    assertEquals(parse(["-a"]), [
+        {
+            _tag: "Flag",
+            name: "a",
+            order: 0,
+            short: true,
+            value: true,
+        },
+    ]);
+
+    assertEquals(parse(["--yes"]), [
+        {
+            _tag: "Flag",
+            name: "yes",
+            order: 0,
+            short: false,
+            value: true,
+        },
+    ]);
+
+    assertEquals(parse(["--no-yes"]), [
+        {
+            _tag: "Flag",
+            name: "no-yes",
+            order: 0,
+            short: false,
+            value: true,
+        },
+    ]);
+});
--- src/model/parse.ts
+++ src/model/parse.ts
@@ -1,4 +1,8 @@
-import { parse as stdParse } from "../libs/flags.ts";
+/**
+ * @copyright Copyright (c) 2022 Adam Josefus
+ */
+
+import { parse as stdParse } from "../../libs/deno_std/flags.ts";
 
 
 interface Argument {
@@ -19,17 +23,16 @@ export interface Flag extends Argument {
 type WithoutTag<T> = Omit<T, "_tag">;
 
 
-const command = (command: WithoutTag<Command>): Command => ({
+const createCommand = (command: WithoutTag<Command>): Command => ({
     ...command,
     _tag: "Command",
 });
 
-const flag = (flag: WithoutTag<Flag>): Flag => ({
+const createFlag = (flag: WithoutTag<Flag>): Flag => ({
     ...flag,
     _tag: "Flag",
 });
 
-const isLongFlag = (arg: string) => arg.startsWith("--");
 
 const isShortFlag = (arg: string) => arg.startsWith("-") && !arg.startsWith("--");
 
@@ -39,72 +42,55 @@ const withoutKey = <T = unknown>(obj: Record<string, T>, key: string) => {
 };
 
 
-const parseShorts = (raw: string, order: number): Flag[] => {
-    const names = raw.slice(1).split("=")[0].split('');
-    console.log(names);
-
-    return names.map(name => flag({
-        name,
-        order,
-        value: true,
-        short: true,
-    }));
-}
-
-
 /**
  * @example
  * ```ts
  * const args = parse(Deno.args());
  * ```
  */
-export const parse = (args: string[]) => {
+export const parse = (args: string[]): ReadonlyArray<Readonly<Flag | Command>> => {
     const commands: Command[] = [];
     const flags: Flag[] = [];
 
     for (let i = 0; i < args.length; i++) {
         const raw = args[i];
 
-        if (isShortFlag(raw)) {
-            const shorts = parseShorts(raw, i);
-            shorts.forEach(flag => flags.push(flag));
-            continue;
-        }
-
         const parsed = stdParse([raw], {
             boolean: true,
+            negatable: [],
+
         });
 
         const alone = parsed._;
-        const [key, value] = Object.entries(withoutKey<unknown>(parsed, "_"))[0] ?? [];
+        const [key, rawValue] = Object.entries(withoutKey<unknown>(parsed, "_"))[0] ?? [];
 
         if (alone.length > 0) {
             const lastFlag = flags.at(-1);
 
-            if (lastFlag && lastFlag.short === false &&  lastFlag.value === true) {
+            if (lastFlag && lastFlag.short === false && lastFlag.value === true) {
                 lastFlag.value = `${alone[0]}`;
             } else {
-                commands.push(command({
+                commands.push(createCommand({
                     name: `${alone[0]}`,
                     order: i,
                 }));
             }
         }
 
+
         if (key !== undefined) {
-            flags.push(flag({
+            const value = rawValue === undefined ? true : typeof rawValue === 'boolean' ? rawValue : `${rawValue}`;
+            const order = i;
+            const short = isShortFlag(raw);
+
+            flags.push(createFlag({
                 name: key,
-                value: value === undefined ? true : typeof value === 'boolean' ? value : `${value}`,
-                order: i,
-                short: isShortFlag(raw),
+                value,
+                order,
+                short,
             }));
         }
     }
 
     return [...commands, ...flags].sort((a, b) => a.order - b.order);
 }
-
-
-const x = parse(Deno.args);
-
-console.log(Deno.args, x);
