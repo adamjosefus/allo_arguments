08687d86e821cd9d005cd95ed998764868c4d03c
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-11-11 15:55:55 +0100

    Update demo

---
+++ demo/demo.ts
@@ -0,0 +1,58 @@
+/**
+ * @copyright Copyright (c) 2022 Adam Josefus
+ */
+
+import { join } from "../libs/deno_std/path.ts";
+import { Arguments, ExpectedException } from "../mod.ts";
+
+
+function getArguments() {
+    const args = new Arguments({
+        ...Arguments.createHelp(),
+        myString: {
+            shortName: 's',
+            description: 'This is a string flag.',
+            convertor: Arguments.stringConvertor,
+        },
+        myNumber: {
+            shortName: 'n',
+            description: 'This is a number flag.',
+            convertor: Arguments.numberConvertor,
+        },
+        myBoolean: {
+            shortName: 'b',
+            description: 'This is a boolean flag.',
+            convertor: Arguments.booleanConvertor,
+        },
+        myCustom: {
+            shortName: 'c',
+            description: 'This is a custom flag.',
+            convertor: value => {
+                if (value === undefined) return undefined;
+
+                return `🐰 — ${value} — 🐭`;
+            },
+        },
+    })
+    .setDesciprion("This is a demo of the arguments library.")
+
+
+    // Important for `--help` flag works.
+    if (args.isHelpRequested()) args.triggerHelp();
+
+    return args.getFlags();
+}
+
+
+try {
+    const args = getArguments();
+
+    console.log(Deno.inspect(args, {
+        colors: true,
+        depth: Infinity,
+        compact: false,
+    }));
+
+} catch (error) {
+    Arguments.rethrowUnprintableException(error);
+}
